f6bf9f5804b5edf674310b1e59ee581052f9068f
Isaac Silva <isaackun98@gmail.com>
2024-03-18 20:11:35 +0900

    Start

---
+++ estud.ia/estud_iaApp.swift
@@ -0,0 +1,17 @@
+//
+//  estud_iaApp.swift
+//  estud.ia
+//
+//  Created by Isaac Silva on 18/03/24.
+//
+
+import SwiftUI
+
+@main
+struct estud_iaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
