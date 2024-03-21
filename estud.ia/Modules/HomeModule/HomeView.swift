7389cad2c3c03585e503ceec1675803b25943a5b
Isaac Silva <isaackun98@gmail.com>
2024-03-21 23:25:51 +0900

    FEAT (HOMEVIEW): Logo, Robot in the middle of the screen, and some texts

--- estud.ia/Modules/HomeModule/HomeView.swift
+++ estud.ia/Modules/HomeModule/HomeView.swift
@@ -11,16 +11,45 @@ struct HomeView: View {
     var body: some View {
         VStack {
             
+            // MARK: Header
             ZStack {
                 Rectangle()
                     .foregroundStyle(Color.logoBackground)
                 
                 VStack{
                     Image("estudiaLogo")
+                        .resizable()
+                        .frame(width: 400,height: 200)
                 }
             }
-            .frame(height: 300)
-        }.ignoresSafeArea()
+            .frame(height: 320)
+            
+            // MARK: Robot image and text
+            VStack{
+                Image("roboHome")
+                    .resizable()
+                    .frame(width: 120, height: 180)
+                    .offset(y: -65)
+                
+                Text("Pergunte")
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .font(.system(size: 30))
+                
+                Text("Inicie uma conversa com o ESTUDIA, o nosso robozinho de IA que ira auxilia-lo em seus estudos")
+                    .font(.system(size: 13))
+                    .multilineTextAlignment(.center)
+                    .lineLimit(2)
+                    .padding(.horizontal, 40)
+                    .opacity(0.7)
+                
+                Text("#estud&Eu")
+                    .padding()
+                    .foregroundStyle(.orange)
+            }
+        }
+        .ignoresSafeArea()
+        
+        Spacer()
     }
 }
 
